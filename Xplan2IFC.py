8b0f25f7d1397ce72d46293dcbe1d97807f1161e
Fatma Ahmad <to.fatmaahmad@gmail.com>
2026-02-09 18:04:02 +0100

    script to read the CRS and XPan data

---
+++ Xplan2IFC.py
@@ -0,0 +1,152 @@
+"""
+This script writes Xplanung and CityGML objects in to the IFC Objects. 
+So that the Architechts have the informed site plan.
+"""
+import math
+import os
+import random
+import xml.etree.ElementTree as ET
+import ifcopenshell
+
+class IFCFloorplanGenerator:
+    """to georeference and create IFC objects."""
+    def __init__ (self, flurstueck_path, Xplanung_path, georeferencing_info_path, tolerance=0.001):
+        self.flurstueck_path = flurstueck_path
+        self.Xplanung_path = Xplanung_path
+        self.ifc_file = ifcopenshell.file(schema="IFC4")
+        self.nsmap = {
+            "gml":  "http://www.opengis.net/gml/3.2",
+            "xplan":"http://www.xplanung.de/xplangml/5/1",
+            "xlink":"http://www.w3.org/1999/xlink",
+            "adv": "http://www.adv-online.de/namespaces/adv/gid/6.0"
+        }
+        self.tolerance = tolerance
+        self.context = None
+        self.UTM_EASTING_ORIGIN = 0.0
+        self.UTM_NORTHING_ORIGIN=0.0
+        self.UTM_HEIGHT_ORIGIN=0.0
+        self.origin_x = 0.0
+        self.origin_y=0.0
+        self.origin_z=0.0
+        self.X_AXIS_ABSCISSA =1
+        self.X_AXIS_ORDINATE =0
+        self.MAP_SCALE = 1.0
+        self.angle= 0.0
+        if georeferencing_info_path:
+            self.load_wld3_values(georeferencing_info_path)
+    def load_wld3_values(self, wld3_path):
+        """It loads the georeferencing data from the wld3 file and calculates 
+        the angle and scale"""
+        with open(wld3_path, "r", encoding="utf-8") as f:
+            lines = [line.strip() for line in f if line.strip()]
+        def parse_line(line):
+            parts = line.replace(",", " ").split()
+            local = tuple(float(x) for x in parts[0:3])
+            utm = tuple(float(x) for x in parts[3:6])
+            return local, utm
+        local1, utm1 = parse_line(lines[0])
+        local2, utm2 = parse_line(lines[1])
+        
+        dx = utm2[0] - utm1[0]
+        dy = utm2[1] - utm1[1]
+
+        self.angle = math.atan2(dy, dx)
+        length = math.hypot(dx, dy)
+        if length != 0:
+            self.X_AXIS_ABSCISSA = dx / length
+            self.X_AXIS_ORDINATE = dy / length
+        else:
+            self.X_AXIS_ABSCISSA = 1.0
+            self.X_AXIS_ORDINATE = 0.0
+
+        self.UTM_EASTING_ORIGIN, self.UTM_NORTHING_ORIGIN, self.UTM_HEIGHT_ORIGIN = utm1
+        self.origin_x, self.origin_y, self.origin_z = utm1
+        d_local = math.dist(local1, local2)
+        d_UTM = math.dist(utm1, utm2)
+        self.MAP_SCALE = d_UTM / d_local if d_local != 0 else 1.0
+    def get_site_geometry_dict(self):
+        """Load Flurstueck, BauLinie, BauGrenze and return their shifted coordinates."""
+        flurstueck_dict = {}
+        baulinie_dict = {}
+        baugrenze_dict = {}
+
+        tree = ET.parse(self.flurstueck_path)
+        root = tree.getroot()
+        for flurstueck in root.findall(".//adv:AX_Flurstueck", self.nsmap):
+            poslist = flurstueck.find(".//gml:posList", self.nsmap)
+            if poslist is not None:
+                coords = [float(x) for x in poslist.text.strip().split()]
+                dim = 3 if len(coords) % 3 == 0 else 2
+                points = []
+                for i in range(0, len(coords), dim):
+                    x = coords[i]
+                    y = coords[i+1]
+                    z = coords[i+2] if dim == 3 else 0.0
+                    points.append([x, y, z])
+
+                shifted_points = []
+                for x, y, z in points:
+                    x_local = (x - self.origin_x) * self.MAP_SCALE
+                    y_local = (y - self.origin_y) * self.MAP_SCALE
+                    z_local = (z - self.origin_z)
+                    x_rot = x_local * self.X_AXIS_ABSCISSA + y_local * self.X_AXIS_ORDINATE
+                    y_rot = -x_local * self.X_AXIS_ORDINATE + y_local * self.X_AXIS_ABSCISSA
+                    shifted_points.append([x_rot, y_rot, z_local])
+
+                flurstueck_id = flurstueck.get("{http://www.opengis.net/gml/3.2}id", "id_not_known")
+                flurstueck_dict[flurstueck_id] = {
+                "name": f"Flurstueck_{flurstueck_id}",
+                "points": shifted_points}
+        tree_xplan = ET.parse(self.Xplanung_path)
+        root_xplan = tree_xplan.getroot()
+        for tag, target_dict, type_name in [("xplan:BP_BauGrenze", baugrenze_dict, "Baugrenze"),
+            ("xplan:BP_BauLinie", baulinie_dict, "Baulinie")]:
+            for element in root_xplan.findall(f".//{tag}", self.nsmap):
+                poslist = element.find(".//gml:posList", self.nsmap)
+                if poslist is not None:
+                    coords = [float(x) for x in poslist.text.strip().split()]
+                    srs_dim = poslist.get("srsDimension")
+                    dim = int(srs_dim) if srs_dim is not None else (3 if len(coords) % 3 == 0 else 2)
+
+                    points = []
+                    for i in range(0, len(coords), dim):
+                        x = coords[i]
+                        y = coords[i+1]
+                        z = coords[i+2] if dim == 3 else 0.0
+                        points.append([x, y, z])
+
+                    shifted_points = []
+                    for x, y, z in points:
+                        x_local = (x - self.origin_x) * self.MAP_SCALE
+                        y_local = (y - self.origin_y) * self.MAP_SCALE
+                        z_local = (z - self.origin_z)
+                        x_rot = x_local * self.X_AXIS_ABSCISSA + y_local * self.X_AXIS_ORDINATE
+                        y_rot = -x_local * self.X_AXIS_ORDINATE + y_local * self.X_AXIS_ABSCISSA
+                        shifted_points.append([x_rot, y_rot, z_local])
+
+                    elem_id = element.get("{http://www.opengis.net/gml/3.2}id", "id_not_found_inGML")
+                    target_dict[elem_id] = {
+                    "name": f"{type_name}_{elem_id}",
+                    "points": shifted_points
+                }
+        return flurstueck_dict, baugrenze_dict, baulinie_dict
+def main():
+    """Main function to run the script"""
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    data_folder = os.path.join(script_dir, "data")
+    flurstueck_gml = os.path.join(data_folder, "flurstueckrequired.gml")
+    xplan_file = os.path.join(data_folder, "XPlanung_lines.gml")
+    wld3_file = os.path.join(data_folder, "Beispieldatei.wld3.wld3")
+    
+    generator = IFCFloorplanGenerator(flurstueck_gml, xplan_file, wld3_file, tolerance=0.001)
+    flurstueck_dict, baugrenze_dict, baulinie_dict = generator.get_site_geometry_dict()
+    print(flurstueck_dict)
+    print(baugrenze_dict)
+    return {
+        "flurstueck_dict": flurstueck_dict,
+        "baugrenze_dict": baugrenze_dict,
+        "baulinie_dict": baulinie_dict,
+        "generator": generator
+    }
+if __name__ == "__main__":
+    main()
---
+++ data/flurstueckrequired.gml
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<wfs:FeatureCollection timeStamp="2025-05-31T07:59:09.535+02:00" numberReturned="1" numberMatched="unknown" xmlns="http://www.adv-online.de/namespaces/adv/gid/6.0" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:wfs="http://www.opengis.net/wfs/2.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.adv-online.de/namespaces/adv/gid/6.0 http://www.wfs.nrw.de/aaa-suite/schema/NAS/6.0.1/schema/AAA-Fachschema.xsd http://www.opengis.net/wfs/2.0 http://www.wfs.nrw.de/aaa-suite/schema/ogc/wfs/2.0/wfs.xsd http://www.opengis.net/gml/3.2 http://www.wfs.nrw.de/aaa-suite/schema/NAS/6.0.1/schema/gml/3.2.1/gml.xsd http://www.isotc211.org/2005/gco http://www.wfs.nrw.de/aaa-suite/schema/NAS/6.0.1/schema/iso/19139/20070417/gco/gco.xsd http://www.isotc211.org/2005/gmd http://www.wfs.nrw.de/aaa-suite/schema/NAS/6.0.1/schema/iso/19139/20070417/gmd/gmd.xsd">
+<wfs:boundedBy>
+<gml:Envelope srsName="urn:ogc:def:crs:EPSG::25832" srsDimension="2">
+<gml:lowerCorner>338131.227 5706237.134</gml:lowerCorner>
+<gml:upperCorner>338161.737 5706279.934</gml:upperCorner>
+</gml:Envelope>
+</wfs:boundedBy>
+<wfs:member>
+<AX_Flurstueck gml:id="DENW21AL100077H6">
+<gml:identifier codeSpace="http://www.adv-online.de/">urn:adv:oid:DENW21AL100077H6</gml:identifier>
+<lebenszeitintervall>
+<AA_Lebenszeitintervall>
+<beginnt>2014-02-14T11:31:00Z</beginnt>
+</AA_Lebenszeitintervall>
+</lebenszeitintervall>
+<modellart>
+<AA_Modellart>
+<advStandardModell>DLKM</advStandardModell>
+</AA_Modellart>
+</modellart>
+<anlass>010400</anlass>
+<zeigtAufExternes>
+<AA_Fachdatenverbindung>
+<art>http://www.bezreg-koeln.nrw.de/extra/33alkis/art.htm#5300</art>
+<fachdatenobjekt>
+<AA_Fachdatenobjekt>
+<name>2009/95001-52</name>
+</AA_Fachdatenobjekt>
+</fachdatenobjekt>
+</AA_Fachdatenverbindung>
+</zeigtAufExternes>
+<zeigtAufExternes>
+<AA_Fachdatenverbindung>
+<art>http://www.bezreg-koeln.nrw.de/extra/33alkis/art.htm#5200</art>
+<fachdatenobjekt>
+<AA_Fachdatenobjekt>
+<name>1954/     -01</name>
+</AA_Fachdatenobjekt>
+</fachdatenobjekt>
+</AA_Fachdatenverbindung>
+</zeigtAufExternes>
+<zeigtAufExternes>
+<AA_Fachdatenverbindung>
+<art>http://www.bezreg-koeln.nrw.de/extra/33alkis/art.htm#8000</art>
+<fachdatenobjekt>
+<AA_Fachdatenobjekt>
+<name>4040051</name>
+</AA_Fachdatenobjekt>
+</fachdatenobjekt>
+</AA_Fachdatenverbindung>
+</zeigtAufExternes>
+<zeigtAufExternes>
+<AA_Fachdatenverbindung>
+<art>urn:adv:fachdatenverbindung:AA_Antrag</art>
+<fachdatenobjekt>
+<AA_Fachdatenobjekt>
+<uri>urn:adv:oid:DENW21AL0000098R</uri>
+</AA_Fachdatenobjekt>
+</fachdatenobjekt>
+</AA_Fachdatenverbindung>
+</zeigtAufExternes>
+<inversZu_dientZurDarstellungVon_AP_PTO xlink:href="urn:adv:oid:DENW21AL100077H5"/>
+<position>
+<gml:Polygon gml:id="o11001.id.76992292.position.Geom_0" srsName="urn:ogc:def:crs:EPSG::25832" srsDimension="2">
+<gml:exterior>
+<gml:LinearRing>
+<gml:posList>338131.227 5706256.699 338144.126 5706242.59 338149.776 5706237.134 338161.737 5706249.234 338156.722 5706279.934 338141.204 5706265.795 338135.397 5706260.503 338131.227 5706256.699</gml:posList>
+</gml:LinearRing>
+</gml:exterior>
+</gml:Polygon>
+</position>
+<gemarkung>
+<AX_Gemarkung_Schluessel>
+<land>05</land>
+<gemarkungsnummer>3290</gemarkungsnummer>
+</AX_Gemarkung_Schluessel>
+</gemarkung>
+<flurstuecksnummer>
+<AX_Flurstuecksnummer>
+<zaehler>85</zaehler>
+</AX_Flurstuecksnummer>
+</flurstuecksnummer>
+<flurstueckskennzeichen>05329001100085______</flurstueckskennzeichen>
+<amtlicheFlaeche uom="m2">684.00</amtlicheFlaeche>
+<flurnummer>11</flurnummer>
+<abweichenderRechtszustand>false</abweichenderRechtszustand>
+<rechtsbehelfsverfahren>false</rechtsbehelfsverfahren>
+<zeitpunktDerEntstehung>1954-01-01</zeitpunktDerEntstehung>
+<gemeindezugehoerigkeit>
+<AX_Gemeindekennzeichen>
+<land>05</land>
+<regierungsbezirk>1</regierungsbezirk>
+<kreis>12</kreis>
+<gemeinde>000</gemeinde>
+</AX_Gemeindekennzeichen>
+</gemeindezugehoerigkeit>
+<istGebucht xlink:href="urn:adv:oid:DENW21AL10007CcG"/>
+<weistAuf xlink:href="urn:adv:oid:DENW21AL100075xc"/>
+</AX_Flurstueck>
+</wfs:member>
+</wfs:FeatureCollection>
